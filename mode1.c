37a3da2d8500191bb5f916aa70dfed33347eaa4d
Raderse <rafael.j.mauricio@gmail.com>
2026-01-31 15:02:59 +0000

    Mode 1 funtion declarations and roadmap

--- mode1.c
+++ mode1.c
@@ -3,3 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void find_error(char **lines, int lines_n, char **dict, int size_dict, char *out_file){
+    // check if each word in line is valid+in dict/within offset of word in dict
+    // for wrong words detected chose based on out_file == NULL to print or to write mistakes
+    if (out_file == NULL){ // No output file (print to terminal)
+
+    }
+    else{ // With output file
+
+    }
+}
--- mode1.h
+++ mode1.h
@@ -1,6 +1,6 @@
 #ifndef MODE1_H
 #define MODE1_H
 
-
+void find_error(char **lines, int lines_n, char **dict, int size_dict, char *out_file);
 
 #endif
