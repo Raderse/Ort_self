7ee8a9e607490733486c65184e1b8aaee44e475e
Raderse <rafael.j.mauricio@gmail.com>
2026-01-31 20:13:04 +0000

    Mode 1 implementation

--- mode1.c
+++ mode1.c
@@ -3,14 +3,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
+// Finds errors in lines and prints/writes them
 void find_error(char **lines, int lines_n, char **dict, int size_dict, char *out_file){
-    // check if each word in line is valid+in dict/within offset of word in dict
-    // for wrong words detected chose based on out_file == NULL to print or to write mistakes
-    if (out_file == NULL){ // No output file (print to terminal)
+    int err_count, pos;
+    char *temp; // Will store word to verify with dict
+    FILE *fptr;
+    fptr = fopen(out_file, "w");
 
-    }
-    else{ // With output file
+    for (int i = 0; i < lines_n; i++){ // Each line
+        temp = malloc(strlen(lines[i]) + 16);
+        if (temp == NULL) exit(EXIT_FAILURE);
 
+        err_count = 0;
+        pos = 0;
+
+        for (int j = 0; lines[i][j] != '\0'; j++){ // Each character
+            if (lines[i][j] != ' '){ // Finish logic to include cases with '
+                temp[pos] = lines[i][j];
+                pos++;
+            }
+            else{
+                if (bsearch(temp, dict, size_dict, sizeof(char *), compare_dic) == NULL){
+                    if (err_count == 0){
+                        if (out_file == NULL){
+                            printf("%d: %s \n", i, lines[i]);
+                        }
+                        else{
+                            fprintf(fptr, "%d: %s \n", i, lines[i]);
+                        }
+                        err_count++;
+                    }
+                    if (out_file == NULL){
+                        printf("Erro na palavra: \"%s\" \n", temp);
+                    }
+                    else{
+                        fprintf(fptr, "%d: %s \n", i, lines[i]);
+                    }
+                }
+                pos = 0;
+            }
+        }
     }
+    free(temp);
+    fclose(fptr);
 }
