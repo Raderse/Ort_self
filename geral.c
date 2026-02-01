dffb7f3b8a70e6357e021820c365e51a6e779b54
Raderse <rafael.j.mauricio@gmail.com>
2026-02-01 08:52:56 +0000

    Bugfix

--- geral.c
+++ geral.c
@@ -18,8 +18,8 @@ int dict_processing(char ***storage, char *file){
     int wordn = 0, size = 10000, len;
     char string[size];
     fptr = fopen(file, "r");
-    if (!(fptr = NULL)){
-        printf("Dictionary could not be opened");
+    if (fptr == NULL){
+        printf("Dictionary could not be opened for processing");
         exit(EXIT_FAILURE);
     }
     while (fgets(string,size,fptr)){
