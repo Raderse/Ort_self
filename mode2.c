7c68f5686eebc6045fc47593201fece60524ab98
Raderse <rafael.j.mauricio@gmail.com>
2026-02-02 18:02:50 +0000

    Base function declaration for mode2 + struct for storing viable alternatives

--- mode2.c
+++ mode2.c
@@ -2,3 +2,7 @@
 #include "geral.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+void suggesting(char **lines, int lines_n, char **dict, int dict_size, char *out_file, int max_diff, int alts){
+
+}
--- mode2.h
+++ mode2.h
@@ -1,6 +1,14 @@
 #ifndef MODE2_H
 #define MODE2_H
 
+typedef struct _Alternative
+{
+    char *word;
+    int diff;
+    int index;
+    struct Alternative *next;
+} Alternative; // type of linked list where *next points to item of index+1
 
+void suggesting(char **lines, int lines_n, char **dict, int dict_size, char *out_file, int max_diff, int alts);
 
 #endif
