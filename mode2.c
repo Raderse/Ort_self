7885fb13108a70a308be7516e95991d7347e46c2
Raderse <rafael.j.mauricio@gmail.com>
2026-02-03 11:27:35 +0000

    New function declarations

--- mode2.c
+++ mode2.c
@@ -6,3 +6,15 @@
 void suggesting(char **lines, int lines_n, char **dict, int dict_size, char *out_file, int max_diff, int alts){
 
 }
+
+Alternative *add_node(Alternative *head, Alternative *new){
+
+}
+
+void free_Alternative(Alternative *head){
+
+}
+
+int in_Alternatives(Alternative *head, char *word){
+
+}
--- mode2.h
+++ mode2.h
@@ -10,5 +10,8 @@ typedef struct _Alternative
 } Alternative; // type of linked list where *next points to item of index+1
 
 void suggesting(char **lines, int lines_n, char **dict, int dict_size, char *out_file, int max_diff, int alts);
+Alternative *add_node(Alternative *head, Alternative *new);
+void free_Alternative(Alternative *head);
+int in_Alternatives(Alternative *head, char *word);
 
 #endif
