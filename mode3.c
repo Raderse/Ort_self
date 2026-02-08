c7c9bd47ce1951094412b6b26d54dad133e7060c
Raderse <rafael.j.mauricio@gmail.com>
2026-02-08 17:48:23 +0000

    Funtion and structure declaration

--- mode3.c
+++ mode3.c
@@ -1,1 +1,6 @@
 #include "geral.h"
+#include "mode2.h"
+
+void replace(char **lines, int lines_n, char **dict, int dict_size, char *out_file, int max_diff){
+    
+}
--- mode3.h
+++ mode3.h
@@ -1,6 +1,12 @@
 #ifndef MODE3_H
 #define MODE3_H
 
+typedef struct Replacement{
+    char *wrong;
+    char *right;
+    struct Replacement *next;
+} Replacement;
 
+void replace(char **lines, int lines_n, char **dict, int dict_size, char *out_file, int max_diff);
 
 #endif
